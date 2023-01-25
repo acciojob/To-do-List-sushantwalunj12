9934cdc5a1fdbfa1a7578d538634609f01f68ba0
Sushant <sushantwalunj12@gmail.com>
2023-01-26 00:28:51 +0530

    Sushant

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //your code here
+const inputTodo = document.getElementById('newTodoInput');
+const todoList = document.getElementById('todoList');
+const addHandler = () => {
+    if (inputTodo.value.trim() === "") {
+        return;
+    }
+    const node = document.createElement('li');
+    const textNode = document.createTextNode(inputTodo.value);
+    node.appendChild(textNode);
+    todoList.appendChild(node);
+    inputTodo.innerText = "";
+    inputTodo.innerHTML = "";
+    inputTodo.value = "";
 
+}
